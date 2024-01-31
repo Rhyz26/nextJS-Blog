a3ea437566f04af70866d1921ef93ff41c457db3
Raymond <njagala.raymond@gmail.com>
2024-01-31 16:36:56 +0300

    first commit

---
+++ app/components/Categories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className='flex gap-6 mb-8'>
+
+    </div>
+  )
+}
+
+export default Categories
---
+++ app/components/Category.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div onClick={() => {}} className='p-4 rounded-lg shadow-md cursor-pointer'>Title</div>
+  )
+}
+
+export default Category
